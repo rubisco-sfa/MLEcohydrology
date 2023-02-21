03e984034222b6fa443b2a2e31e14f5c043a4e08
Nathan Collier <nathaniel.collier@gmail.com>
2023-02-20 19:51:29 -0500

    added a plot for rf

--- models/random_forest_maat.py
+++ models/random_forest_maat.py
@@ -3,6 +3,8 @@
 import pickle
 
 import intake
+import matplotlib.pyplot as plt
+import numpy as np
 from common import remove_outliers
 from sklearn.ensemble import RandomForestRegressor
 
@@ -20,3 +22,13 @@ regr = RandomForestRegressor(random_state=RANDOM_STATE, bootstrap=True)
 regr.fit(x, y)
 with open("random_forest_regressor.pkl", "wb") as pkl:
     pickle.dump(regr, pkl)
+
+fig, ax = plt.subplots(figsize=(7, 7), tight_layout=True)
+prediction = regr.predict(x)
+ax.scatter(y, prediction)
+ax.plot([0, y.max()], [0, y.max()], "--k")
+ax.axis("equal")
+ax.set_xlabel("Lin2015 Cond")
+ax.set_ylabel("Random Forest Cond")
+ax.set_title(f"$R^{2}$ = {np.corrcoef(prediction,y)[0,1]:.3f}")
+fig.savefig("cond_rf.png")
